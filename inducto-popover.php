0bc2faab491c65447661d56bd78d9fb1e55ca856
Marie <marie@violetco.de>
2015-12-03 11:22:57 -0800

    Updated to store and have proper menu

--- inducto-popover.php
+++ inducto-popover.php
@@ -10,24 +10,26 @@ Version: 1.0
 Author URI: http://twelve23.com
 */
 
-add_action('admin_menu', 'add_global_custom_options');
+add_action('admin_menu', 'add_inductopop_options');
 
-function add_global_custom_options () {
-	add_options_page('Global Custom Fields', 'Global Custom Fields', 'manage_options', 'functions', 'global_custom_options');
+function add_inductopop_options () {
+	add_menu_page('Inductopop', 'Inductopop', 'manage_options', 'functions', 'inductopop_options');
 
 }
 
-function global_custom_options() {
+function inductopop_options() {
 	?>
 	<div class="wrap">
-        <h2>Global Custom Options</h2>
+        <h2>Inductotherm Popover Options</h2>
         <form method="post" action="options.php">
             <?php wp_nonce_field('update-options') ?>
             <p><strong>Title:</strong><br />
                 <input type="text" name="popup_title" size="45" value="<?php echo get_option('popup_title'); ?>" />
             </p>
             <p><strong>Description:</strong><br />
-                <input type="text" name="popup_description" size="45" value="<?php echo get_option('popup_description'); ?>" />
+                <?php wp_editor(get_option('popup_description'), 'popup_description', 
+                	$settings = array('textarea_name' => 'popup_description'));
+            	?>
             </p>
             <p><input type="submit" name="Submit" value="Store Options" /></p>
             <input type="hidden" name="action" value="update" />
@@ -35,5 +37,12 @@ function global_custom_options() {
         </form>
     </div>
     <?php
+}
+
+function inductopop() {
+?>
+	<h1><?php echo get_option("popup_title");?></h1>
+	<p><?php echo get_option("popup_description");?></p>
+<?php	
 }
 ?>
